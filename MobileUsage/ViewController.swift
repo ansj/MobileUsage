d428b15163f967c89eddca225c3e21b55395c6a3
sjartuni <a12123@hotmail.com>
2019-09-05 22:41:38 +0800

    Initial Commit

---
+++ MobileUsage/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MobileUsage
+//
+//  Created by Ananta Sjartuni on 5/9/19.
+//  Copyright © 2019 Ananta Sjartuni. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
